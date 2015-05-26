f23f32880982438a928c5b3062ca7806ab61f612
Ustes <ustes.greenridge@gmail.com>
2015-05-26 12:32:13 -0700

    initial project

---
+++ RibbonTfs.Designer.vb
@@ -0,0 +1,198 @@
+﻿Partial Class RibbonTfs
+    Inherits Microsoft.Office.Tools.Ribbon.RibbonBase
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+   Public Sub New(ByVal container As System.ComponentModel.IContainer)
+        MyClass.New()
+
+        'Required for Windows.Forms Class Composition Designer support
+        If (container IsNot Nothing) Then
+            container.Add(Me)
+        End If
+
+    End Sub
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Public Sub New()
+        MyBase.New(Globals.Factory.GetRibbonFactory())
+
+        'This call is required by the Component Designer.
+        InitializeComponent()
+
+    End Sub
+
+    'Component overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Component Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Component Designer
+    'It can be modified using the Component Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Tab1 = Me.Factory.CreateRibbonTab
+        Me.Group4 = Me.Factory.CreateRibbonGroup
+        Me.Group2 = Me.Factory.CreateRibbonGroup
+        Me.lblSvr = Me.Factory.CreateRibbonLabel
+        Me.lblTFSInfo = Me.Factory.CreateRibbonLabel
+        Me.Group1 = Me.Factory.CreateRibbonGroup
+        Me.cboWorkItemType = Me.Factory.CreateRibbonComboBox
+        Me.cboArea = Me.Factory.CreateRibbonComboBox
+        Me.cboIteration = Me.Factory.CreateRibbonComboBox
+        Me.cboAssignedTo = Me.Factory.CreateRibbonComboBox
+        Me.Group3 = Me.Factory.CreateRibbonGroup
+        Me.btnSelectProject = Me.Factory.CreateRibbonButton
+        Me.btnCreate = Me.Factory.CreateRibbonButton
+        Me.Separator1 = Me.Factory.CreateRibbonSeparator
+        Me.Tab1.SuspendLayout()
+        Me.Group4.SuspendLayout()
+        Me.Group2.SuspendLayout()
+        Me.Group1.SuspendLayout()
+        Me.Group3.SuspendLayout()
+        '
+        'Tab1
+        '
+        Me.Tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office
+        Me.Tab1.Groups.Add(Me.Group4)
+        Me.Tab1.Groups.Add(Me.Group2)
+        Me.Tab1.Groups.Add(Me.Group1)
+        Me.Tab1.Groups.Add(Me.Group3)
+        Me.Tab1.Label = "TabAddIns"
+        Me.Tab1.Name = "Tab1"
+        '
+        'Group4
+        '
+        Me.Group4.Items.Add(Me.btnSelectProject)
+        Me.Group4.Label = "TFS Server"
+        Me.Group4.Name = "Group4"
+        '
+        'Group2
+        '
+        Me.Group2.Items.Add(Me.lblSvr)
+        Me.Group2.Items.Add(Me.lblTFSInfo)
+        Me.Group2.Label = "TFS Info"
+        Me.Group2.Name = "Group2"
+        '
+        'lblSvr
+        '
+        Me.lblSvr.Label = "TFS Server"
+        Me.lblSvr.Name = "lblSvr"
+        '
+        'lblTFSInfo
+        '
+        Me.lblTFSInfo.Label = "Project name"
+        Me.lblTFSInfo.Name = "lblTFSInfo"
+        '
+        'Group1
+        '
+        Me.Group1.Items.Add(Me.cboWorkItemType)
+        Me.Group1.Items.Add(Me.cboArea)
+        Me.Group1.Items.Add(Me.cboIteration)
+        Me.Group1.Items.Add(Me.Separator1)
+        Me.Group1.Items.Add(Me.cboAssignedTo)
+        Me.Group1.Label = "Work Item Info"
+        Me.Group1.Name = "Group1"
+        '
+        'cboWorkItemType
+        '
+        Me.cboWorkItemType.Label = "Work Item Type"
+        Me.cboWorkItemType.Name = "cboWorkItemType"
+        Me.cboWorkItemType.Text = Nothing
+        '
+        'cboArea
+        '
+        Me.cboArea.Label = "Area"
+        Me.cboArea.Name = "cboArea"
+        Me.cboArea.Text = Nothing
+        '
+        'cboIteration
+        '
+        Me.cboIteration.Label = "Iteration"
+        Me.cboIteration.Name = "cboIteration"
+        Me.cboIteration.Text = Nothing
+        '
+        'cboAssignedTo
+        '
+        Me.cboAssignedTo.Label = "Assigned To"
+        Me.cboAssignedTo.Name = "cboAssignedTo"
+        '
+        'Group3
+        '
+        Me.Group3.Items.Add(Me.btnCreate)
+        Me.Group3.Label = "Actions"
+        Me.Group3.Name = "Group3"
+        '
+        'btnSelectProject
+        '
+        Me.btnSelectProject.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge
+        Me.btnSelectProject.Image = Global.TfsOutlookAddIn.My.Resources.Resources.magnifier5
+        Me.btnSelectProject.Label = "Select Project"
+        Me.btnSelectProject.Name = "btnSelectProject"
+        Me.btnSelectProject.ShowImage = True
+        '
+        'btnCreate
+        '
+        Me.btnCreate.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge
+        Me.btnCreate.Image = Global.TfsOutlookAddIn.My.Resources.Resources.writing9
+        Me.btnCreate.Label = "Create Work Item"
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.ShowImage = True
+        '
+        'Separator1
+        '
+        Me.Separator1.Name = "Separator1"
+        '
+        'RibbonTfs
+        '
+        Me.Name = "RibbonTfs"
+        Me.RibbonType = "Microsoft.Outlook.Mail.Read"
+        Me.Tabs.Add(Me.Tab1)
+        Me.Tab1.ResumeLayout(False)
+        Me.Tab1.PerformLayout()
+        Me.Group4.ResumeLayout(False)
+        Me.Group4.PerformLayout()
+        Me.Group2.ResumeLayout(False)
+        Me.Group2.PerformLayout()
+        Me.Group1.ResumeLayout(False)
+        Me.Group1.PerformLayout()
+        Me.Group3.ResumeLayout(False)
+        Me.Group3.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Tab1 As Microsoft.Office.Tools.Ribbon.RibbonTab
+    Friend WithEvents Group2 As Microsoft.Office.Tools.Ribbon.RibbonGroup
+    Friend WithEvents btnCreate As Microsoft.Office.Tools.Ribbon.RibbonButton
+    Friend WithEvents cboWorkItemType As Microsoft.Office.Tools.Ribbon.RibbonComboBox
+    Friend WithEvents btnSelectProject As Microsoft.Office.Tools.Ribbon.RibbonButton
+    Friend WithEvents lblTFSInfo As Microsoft.Office.Tools.Ribbon.RibbonLabel
+    Friend WithEvents Group1 As Microsoft.Office.Tools.Ribbon.RibbonGroup
+    Friend WithEvents lblSvr As Microsoft.Office.Tools.Ribbon.RibbonLabel
+    Friend WithEvents cboArea As Microsoft.Office.Tools.Ribbon.RibbonComboBox
+    Friend WithEvents cboIteration As Microsoft.Office.Tools.Ribbon.RibbonComboBox
+    Friend WithEvents Group4 As Microsoft.Office.Tools.Ribbon.RibbonGroup
+    Friend WithEvents cboAssignedTo As Microsoft.Office.Tools.Ribbon.RibbonComboBox
+    Friend WithEvents Group3 As Microsoft.Office.Tools.Ribbon.RibbonGroup
+    Friend WithEvents Separator1 As Microsoft.Office.Tools.Ribbon.RibbonSeparator
+End Class
+
+Partial Class ThisRibbonCollection
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Friend ReadOnly Property RibbonTfs() As RibbonTfs
+        Get
+            Return Me.GetRibbon(Of RibbonTfs)()
+        End Get
+    End Property
+End Class
---
+++ RibbonTfs.vb
@@ -0,0 +1,245 @@
+﻿Imports Microsoft.Office.Tools.Ribbon
+Imports System
+Imports System.Collections.ObjectModel
+Imports Microsoft.TeamFoundation.Client
+Imports Microsoft.TeamFoundation.Framework.Common
+Imports Microsoft.TeamFoundation.Framework.Client
+Imports Microsoft.TeamFoundation.Proxy
+Imports Microsoft.TeamFoundation.Server
+Imports Microsoft.TeamFoundation.WorkItemTracking.Client
+Imports Microsoft.TeamFoundation.WorkItemTracking.WpfControls
+Imports Microsoft.Office.Interop.Outlook
+Imports System.Collections
+Imports System.IO
+
+Public Class RibbonTfs
+    Private configurationServer As TfsConfigurationServer = Nothing
+    Private currentProject As TfsTeamProjectCollection = Nothing
+    Private currentWorkItemStore As WorkItemStore = Nothing
+    Private currentProjectName As String
+    Private currentUser As String
+
+    Private Sub RibbonTfs_Load(ByVal sender As System.Object, ByVal e As RibbonUIEventArgs) Handles MyBase.Load
+
+        Dim projUri As String = GetSetting("tfsOutlookAddin", "TFS", "Server", "")
+        Dim svrName As String = GetSetting("tfsOutlookAddin", "TFS", "ServerName", "").Replace("Server : ", "")
+        Dim projName As String = GetSetting("tfsOutlookAddin", "TFS", "ProjectName", "").Replace("Project : ", "")
+        If Not String.IsNullOrEmpty(projUri) Then
+            Dim u As New Uri(projUri)
+            currentProject = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(u)
+            lblSvr.Label = svrName
+            lblTFSInfo.Label = projName
+
+            lblSvr.Label = "Server : " & svrName
+            lblTFSInfo.Label = "Project : " & projName
+
+
+            currentWorkItemStore = currentProject.GetService(Of WorkItemStore)()
+            loadOptions(projName)
+
+        End If
+
+        Dim item As inspector = TryCast(Me.Context, Inspector)
+
+        currentUser = item.Application.Session.CurrentUser.Name
+
+    End Sub
+
+    Private Sub btnCreate_Click(sender As Object, e As RibbonControlEventArgs) Handles btnCreate.Click
+        Try
+            Dim wit As String = cboWorkItemType.Text
+            Dim selectedUser As String = cboAssignedTo.Text
+            currentProjectName = lblTFSInfo.Label.Replace("Project :", "").Trim
+
+            If String.IsNullOrEmpty(selectedUser) Then
+                selectedUser = currentUser
+            End If
+
+            If Not String.IsNullOrEmpty(wit) And Not String.IsNullOrEmpty(currentProjectName) Then
+                Dim item As inspector = TryCast(Me.Context, Inspector)
+                Dim mail As MailItem = TryCast(item.CurrentItem, MailItem)
+
+                Dim workItemTypes As WorkItemTypeCollection = currentWorkItemStore.Projects(currentProjectName).WorkItemTypes
+
+                Dim sw As WorkItemType = workItemTypes(wit)
+                Dim wi As New WorkItem(sw)
+                wi.Title = mail.Subject
+                wi.Description = mail.HTMLBody.ToString
+              
+                wi.Fields("Assigned to").Value = selectedUser
+
+
+                Dim mailPath As String = String.Format("{0}{1}.msg", Path.GetTempPath, Path.GetFileNameWithoutExtension(Path.GetRandomFileName()))
+                mail.SaveAs(mailPath)
+
+
+                Dim wiAttach As New Microsoft.TeamFoundation.WorkItemTracking.Client.Attachment(mailPath, mail.Subject)
+                wi.Attachments.Add(wiAttach)
+
+                For Each att As Microsoft.Office.Interop.Outlook.Attachment In mail.Attachments
+                    Try
+
+                        Dim attPath As String = String.Format("{0}{1}", Path.GetTempPath, att.FileName)
+                        att.SaveAsFile(attPath)
+                        Dim wiAtt As New Microsoft.TeamFoundation.WorkItemTracking.Client.Attachment(attPath, att.DisplayName)
+                        wi.Attachments.Add(wiAtt)
+                    Catch ex As System.Exception
+                        Continue For
+                    End Try
+
+                Next
+
+                Dim errorMessage As String = ""
+                For Each field As Field In wi.Fields
+                    If field.Status <> FieldStatus.Valid Then
+                        errorMessage = errorMessage & String.Format("{0} - {1} = {2}", field.Status.ToString, field.ReferenceName, field.Value) & Environment.NewLine
+                    End If
+                Next
+                If Not String.IsNullOrEmpty(errorMessage) Then
+                    MsgBox(errorMessage)
+                    Exit Sub
+                End If
+
+                Dim errs As ArrayList = wi.Validate
+                If errs.Count = 0 Then
+                    Try
+                        wi.Save()
+                        Dim wiUrl As String = String.Format("{0}", wi.Uri.PathAndQuery)
+                        'System.Diagnostics.Process.Start(wiUrl)
+                        MsgBox(String.Format("Work Item #{0} Successfully Created!!!", wi.Id.ToString))
+
+                    Catch ex As TeamFoundationPropertyValidationException
+                        MsgBox(ex.Message.ToString)
+                    End Try
+
+                Else
+                    Dim errMsg As New StringBuilder
+                    For Each fld As Field In errs
+                        errMsg.AppendLine(String.Format("{0}", fld.ReferenceName) & Environment.NewLine)
+                    Next
+                    MsgBox("Validation Errors: " & errMsg.ToString)
+                End If
+
+            End If
+
+        Catch ex As System.Exception
+            MsgBox(ex.Message.ToString, vbOKOnly, "tfsOutlookAddin")
+        End Try
+      
+    End Sub
+
+    Private Sub btnSelectProject_Click(sender As Object, e As RibbonControlEventArgs) Handles btnSelectProject.Click
+
+        'If IsNothing(currentProject) Then
+        Dim dp As New TeamProjectPicker(TeamProjectPickerMode.SingleProject, False)
+        dp.ShowDialog()
+        For Each p As ProjectInfo In dp.SelectedProjects
+            currentProjectName = p.Name
+            Exit For
+        Next
+        currentProject = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(dp.SelectedTeamProjectCollection.Uri)
+
+        lblSvr.Label = "Server : " & currentProject.ToString
+        lblTFSInfo.Label = "Project : " & currentProjectName
+        SaveSetting("tfsOutlookAddin", "TFS", "ServerName", lblSvr.Label)
+        SaveSetting("tfsOutlookAddin", "TFS", "ProjectName", lblTFSInfo.Label)
+        SaveSetting("tfsOutlookAddin", "TFS", "Server", currentProject.Uri.ToString)
+        'End If
+
+        currentWorkItemStore = currentProject.GetService(Of WorkItemStore)()
+
+
+        loadOptions(currentProjectName)
+
+
+
+    End Sub
+
+    Private Sub loadOptions(projectName As String)
+        Dim newrib As RibbonDropDownItem
+
+        cboWorkItemType.Items.Clear()
+        cboArea.Items.Clear()
+        cboIteration.Items.Clear()
+        For Each w As WorkItemType In currentWorkItemStore.Projects(projectName).WorkItemTypes
+            newrib = Me.Factory.CreateRibbonDropDownItem()
+            newrib.Label = w.Name
+            cboWorkItemType.Items.Add(newrib)
+        Next
+
+
+        For Each w As Node In currentWorkItemStore.Projects(projectName).AreaRootNodes
+            newrib = Me.Factory.CreateRibbonDropDownItem()
+            newrib.Label = w.Path
+            cboArea.Items.Add(newrib)
+            For Each c As Node In w.ChildNodes
+                newrib = Me.Factory.CreateRibbonDropDownItem()
+                newrib.Label = c.Path
+                cboArea.Items.Add(newrib)
+            Next
+        Next
+
+
+
+        For Each w As Node In currentWorkItemStore.Projects(projectName).IterationRootNodes
+            newrib = Me.Factory.CreateRibbonDropDownItem()
+            newrib.Label = w.Path
+            cboIteration.Items.Add(newrib)
+            For Each c As Node In w.ChildNodes
+                newrib = Me.Factory.CreateRibbonDropDownItem()
+                newrib.Label = c.Path
+                cboIteration.Items.Add(newrib)
+            Next
+        Next
+
+
+
+
+        Dim identityManagementService As Microsoft.TeamFoundation.Framework.Client.IIdentityManagementService = currentProject.GetService(Of IIdentityManagementService)()
+        Dim collectionWideValidUsers As TeamFoundationIdentity = identityManagementService.ReadIdentity(IdentitySearchFactor.DisplayName,
+                                                                              "Project Collection Valid Users",
+                                                                              MembershipQuery.Expanded,
+                                                                              ReadIdentityOptions.None)
+
+        Dim validMembers As TeamFoundationIdentity() = identityManagementService.ReadIdentities(collectionWideValidUsers.Members,
+                                                                    MembershipQuery.Expanded,
+                                                                    ReadIdentityOptions.ExtendedProperties)
+
+        Dim memberNames As List(Of String) = validMembers.Where(Function(t As TeamFoundationIdentity) Not t.IsContainer _
+                                                                    And Not t.DisplayName.Contains("Microsoft") _
+                                                                    And Not t.DisplayName.Contains("TeamFoundationService") _
+                                                                    And t.Descriptor.IdentityType <> "Microsoft.TeamFoundation.UnauthenticatedIdentity" _
+                                                                    ).Select(Function(t) t.DisplayName).ToList()
+
+        For Each m As String In memberNames
+            newrib = Me.Factory.CreateRibbonDropDownItem()
+            newrib.Label = m
+            cboAssignedTo.Items.Add(newrib)
+        Next
+
+
+
+        If cboArea.Items.Count = 0 Then
+            newrib = Me.Factory.CreateRibbonDropDownItem()
+            newrib.Label = projectName
+            cboArea.Items.Add(newrib)
+        End If
+
+        If cboIteration.Items.Count = 0 Then
+            newrib = Me.Factory.CreateRibbonDropDownItem()
+            newrib.Label = projectName
+            cboIteration.Items.Add(newrib)
+        End If
+        If cboIteration.Items.Count = 0 Then
+            newrib = Me.Factory.CreateRibbonDropDownItem()
+            newrib.Label = currentUser
+            cboAssignedTo.Items.Add(newrib)
+        End If
+
+
+
+
+
+    End Sub
+
+End Class
